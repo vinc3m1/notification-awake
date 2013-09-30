aa9845e2b5932680b0dc569d1d35cbb68a4f37d7
Vince Mi <vinc3m1@gmail.com>
2013-09-29 23:18:11 -0700

    Fix exception, runs now, timeout still default, not based on wakelock

--- src/com/makeramen/notificationawake/NotificationListener.java
+++ src/com/makeramen/notificationawake/NotificationListener.java
@@ -6,7 +6,6 @@ import android.os.PowerManager;
 import android.preference.PreferenceManager;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
-import android.util.Log;
 
 public class NotificationListener extends NotificationListenerService {
 
@@ -15,35 +14,30 @@ public class NotificationListener extends NotificationListenerService {
     @Override
     public void onCreate() {
         super.onCreate();
-        Log.d("vmi", "onCreate Service");
         mHandler = new Handler();
     }
 
     @Override
     public void onDestroy() {
-        Log.d("vmi", "onDestroy Service");
         super.onDestroy();
     }
 
     @Override
     public void onNotificationPosted(StatusBarNotification sbn) {
-        Log.d("vmi", "onNotificationPosted");
-
         PowerManager pm = (PowerManager) this.getSystemService(Context.POWER_SERVICE);
         final PowerManager.WakeLock wakeLock = pm.newWakeLock(PowerManager.ACQUIRE_CAUSES_WAKEUP | PowerManager.SCREEN_BRIGHT_WAKE_LOCK, "NotificationAwake");
-        Log.d("vmi", "acquire wakeLock");
         wakeLock.acquire();
+
+        int delay = Integer.valueOf(PreferenceManager.getDefaultSharedPreferences(this).getString("timeout", "5")) * 1000;
 
         mHandler.postDelayed(new Runnable() {
             @Override public void run() {
-                Log.d("vmi", "release wakeLock");
                 wakeLock.release();
             }
-        }, PreferenceManager.getDefaultSharedPreferences(this).getInt("timeout", 5) * 1000);
+        }, delay);
     }
 
-    @Override
-    public void onNotificationRemoved(StatusBarNotification sbn) {
+    @Override public void onNotificationRemoved(StatusBarNotification sbn) {
         // meh
     }
 }
